17a1c8d9c3aebc8c302d11848b2690ed339125a3
david <drobles988@gmail.com>
2022-07-04 21:51:12 +0200

    feat: preparamos la app balance

---
+++ balance/views.py
@@ -0,0 +1,19 @@
+from balance import app
+
+@app.route('/')
+def home():
+    return "Pagina de inicio "
+
+@app.route('/nuevo')
+def nuevo():
+    return "Creacion de movimiento"
+
+
+@app.route('/modificar')
+def actualizar():
+    return "Actualizar movimiento"
+
+
+@app.route('/borrar')
+def borrar():
+    return "Borrar movimiento"
