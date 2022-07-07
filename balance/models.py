66e821c1868ae723df9d733bd6e646d55abd1676
david <drobles988@gmail.com>
2022-07-07 12:19:16 +0200

    feat: control de errores en el formato de fecha

--- balance/models.py
+++ balance/models.py
@@ -1,22 +1,34 @@
 import csv
+from datetime import date, datetime
 from . import FICHERO
+
 
 class Movimiento:
     def __init__(self, fecha, concepto, tipo, cantidad):
-        self.fecha = fecha
+        self.errores = []
+        try:
+            self.fecha = date.fromisoformat(fecha)
+        except ValueError:
+            self.fecha = None
+            self.errores.append("El formato de la fecha no es valido")
+
         self.concepto = concepto
         self.tipo = tipo
         self.cantidad = cantidad
 
+   
+
 
 class ListaMovimientos:
-    def  __init__(self):
-        self.lista_movimientos = []
+    def __init__(self):
+        self.movimientos = []
 
     def leer_archivo(self):
         with open(FICHERO, "r") as fichero:
             reader = csv.DictReader(fichero)
             for linea in reader:
-                self.lista_movimientos.append(linea)
-
-
+                #self.movimientos.append(linea)
+                mov = Movimiento(
+                    linea["fecha"], linea["concepto"], 
+                    linea["tipo"], linea["cantidad"])
+                self.movimientos.append(mov)
