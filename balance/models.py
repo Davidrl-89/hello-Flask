d070db23ac0161451ac4bea5185e8a0591392a1e
david <drobles988@gmail.com>
2022-07-10 17:24:49 +0200

    feat: primera version de agregar movimiento

--- balance/models.py
+++ balance/models.py
@@ -1,7 +1,8 @@
 import csv
-from datetime import date, datetime
+from datetime import date
 from . import FICHERO
 
+CLAVES_IGNORADAS = ["errores"]
 
 class Movimiento:
     def __init__(self, dic_datos):
@@ -24,10 +25,6 @@ class Movimiento:
             self.cantidad
         )
 
-
-   
-
-
 class ListaMovimientos:
     def __init__(self):
         self.movimientos = []
@@ -39,3 +36,28 @@ class ListaMovimientos:
                 #self.movimientos.append(linea)
                 mov = Movimiento(linea)
                 self.movimientos.append(mov)
+
+    def agregar(self, movimiento):
+        self.movimientos.append(movimiento)
+        self.guardar_archivo()
+
+    def guardar_archivo(self):
+        nombres = list(self.movimientos[0].__dict__.keys())
+        for nombre in nombres:
+            if nombre == CLAVES_IGNORADAS:
+                nombres.remove(nombre)
+
+        with open(FICHERO, "w") as fichero:
+            writer = csv.DictWriter(fichero, nombres)
+            writer.writeheader()
+            for mov in self.movimientos:
+                dic_mov = mov.__dict__
+                for nombre in CLAVES_IGNORADAS:
+                    dic_mov.pop(nombre)
+                writer.writerow(dic_mov)
+
+    def __str__(self):
+        return f"Lista de {len(self.movimientos)} movimientos"
+
+    def __repr__(self) -> str:
+        return self.__str__()
